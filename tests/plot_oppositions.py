b3e098c33aaf9b6c7290113c932085702d5a62de
ДМ. Па <qtckpuhdsa@gmail.com>
2020-12-15 14:10:37 +0300

    -

--- tests/plot_oppositions.py
+++ tests/plot_oppositions.py
@@ -1,9 +1,11 @@
+
+import random
 import numpy as np
 import matplotlib.pyplot as plt 
 
 
 
-def plot_opposition(points, oppositions, bounds, title, net = False, save_as = None):
+def plot_oppositions(point, oppositions, bounds, names, title, net = False, save_as = None):
     
     xmin, xmax, ymin, ymax = bounds[0,0],bounds[0,1], bounds[1,0], bounds[1,1]
     plt.axis((xmin, xmax, ymin, ymax))
@@ -18,15 +20,21 @@ def plot_opposition(points, oppositions, bounds, title, net = False, save_as = N
         for yc in ys:
             plt.axhline(y=yc, color='k', linestyle='--', lw = 0.3)
 
-    
-    for i in range(points.shape[0]):
+    xg = (xmax-xmin)
+    yg = (ymax-ymin)    
 
-        p1 = points[i, :]
+    for i in range(oppositions.shape[0]):
+
+        p1 = point
         p2 = oppositions[i, :]
     
         plt.plot([p1[0],p2[0]], [p1[1],p2[1]], 'r--', color = 'blue')
+        plt.annotate(names[i], (p2[0],p2[1]), 
+            xytext=(p2[0]+np.random.uniform(-1,1)*0.2*xg, p2[1]+np.random.uniform(-1,1)*0.1*yg),
+                     arrowprops = dict(arrowstyle="fancy"),
+                     bbox=dict(boxstyle="round", fc="w"))
 
-    plt.plot(points[:, 0], points[:, 1], 'ro', color = 'green', label = 'current points', markeredgecolor = "black")
+    plt.plot(point[0], point[1], 'ro', color = 'green', label = 'current points', markeredgecolor = "black")
     plt.plot(oppositions[:, 0], oppositions[:, 1], 'ro', color = 'red', label = 'points oppositions', markeredgecolor = "black")
 
     
@@ -37,7 +45,7 @@ def plot_opposition(points, oppositions, bounds, title, net = False, save_as = N
         return (xmin*rx + (1-rx)*xmax, ymin*ry + (1-ry)*ymax)
 
     plt.annotate('center of zone', xy = ((xmin+xmax)/2, (ymin+ymax)/2), 
-                 xytext = random_coords(xmin, xmax, ymin, ymax, wd = 0.4) ,
+                 xytext = random_coords(xmin, xmax, ymin, ymax, wd = 0.3) ,
              arrowprops=dict(facecolor='red', shrink=0.05), 
               #color = 'red',
               bbox = dict(boxstyle ="round", fc ="0.8")#, arrowprops = arrowprops
@@ -60,11 +68,7 @@ def plot_opposition(points, oppositions, bounds, title, net = False, save_as = N
 
 if __name__ == '__main__':
     
-    p = np.array([
-        [1, 3],
-        [4, 6],
-        [9, 7]
-    ])
+    p = np.array([1, 3])
 
     op = np.array([
         [3, 1],
@@ -77,4 +81,6 @@ if __name__ == '__main__':
         [0, 11]
     ])
 
-    plot_opposition(p, op, bounds, title = "dfsfwe", net = True)
+    names = ['1', '2', '3']
+
+    plot_oppositions(p, op, bounds, names, title = "opop", net = True)
